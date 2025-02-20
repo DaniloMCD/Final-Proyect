4337111ccd895c2df5bc053b87023354dfcf192c
DaniloMCD <danilo.granados2026@hilasal.superate.org.sv>
2025-02-19 22:57:29 -0600

    Ya pregunta si quiere jugar de nuevo!

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ def russianRoulete(a,b):
         b = random.randint(1,5)
 
         if a == b:
-              print("Moriste")
+            print("-"*7)
+            print("|Moriste|")
+            print("-"*7)
         else:
-             print(f"Pura suerte, la bala estaba en el cartucho {b} y disparaste el cartucho {a} \n")
+            print("-"*74)
+            print(f" |  Casi!! la bala estaba en el cartucho {b} y disparaste el cartucho {a}  | ")
+            print("-"*74)
 a=0
 b=0
 #Historial
@@ -15,13 +19,20 @@ story = []
 #Menu
 
 while(True):
-    print("Bienvenidoo \n")
+    print("+" + "-"*12 + "+")
+    print("| BIENVENIDO |")
+    print("+" + "-"*12 + "+")
     print("Este es el juego de la ruleta rusa, ganas si vives, pierdes si mueres... \n")
     print("O si solo quieres ver el historial pon 1 \n")
-    response = (input("Te atreves a jugar? "))
+    response = input("Te atreves a jugar? ")
 
     if response == "Si":
         russianRoulete(a,b)
+        response2 = input("Quieres seguir jugando? ")
+        if response2 == "si":
+            print("OK")
+        else:
+            break
     else:
         break
 
