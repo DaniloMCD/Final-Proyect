9a0c58321ea6adfb5a3671f04c8fb97d71daac59
DaniloMCD <danilo.granados2026@hilasal.superate.org.sv>
2025-02-19 22:36:48 -0600

    Medio avancezzzz al menos ya sale si mori o no

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("hi")
+import random
+
+def russianRoulete(a,b):
+        a = random.randint(1,5)
+        b = random.randint(1,5)
+
+        if a == b:
+              print("Moriste")
+        else:
+             print(f"Pura suerte, la bala estaba en el cartucho {b} y disparaste el cartucho {a} \n")
+a=0
+b=0
+#Historial
+story = []
+#Menu
+
+while(True):
+    print("Bienvenidoo \n")
+    print("Este es el juego de la ruleta rusa, ganas si vives, pierdes si mueres... \n")
+    print("O si solo quieres ver el historial pon 1 \n")
+    response = (input("Te atreves a jugar? "))
+
+    if response == "Si":
+        russianRoulete(a,b)
+    else:
+        break
+
+    
+
+
+    
